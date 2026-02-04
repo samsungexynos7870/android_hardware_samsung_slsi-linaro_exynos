88334fcdf8b508d42384716cb7ea72221d213648
Flominator <github.flominatorgd@gmail.com>
2026-02-04 17:52:34 +0100

    libcamera: update dump from ExynosCamera
    android 8 libexynoscamera3 has android::ExynosCamera::dump(int) const instead of android::ExynosCamera::dump(int)

--- libcamera/7870/hal1/ExynosCamera.h
+++ libcamera/7870/hal1/ExynosCamera.h
@@ -186,7 +186,7 @@ public:
     status_t    storeMetaDataInBuffers(bool enable);
 
     /* For debugging */
-    status_t    dump(int fd);
+    status_t    dump(int fd) const;
 
 private:
     /* Internal Threads & Functions */
