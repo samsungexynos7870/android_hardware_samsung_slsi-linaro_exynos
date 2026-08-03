8967af154cabee2c98adaab824c7815b6ab5e7c5
Flominator <github.flominatorgd@gmail.com>
2026-08-03 22:16:29 +0200

    libcamera: set IMX258 id 110
    switch with IMX386

--- libcamera/common_v2/ExynosCameraCommonEnum.h
+++ libcamera/common_v2/ExynosCameraCommonEnum.h
@@ -56,8 +56,8 @@ typedef enum
     SENSOR_NAME_IMX228              = 106,
     SENSOR_NAME_IMX219              = 107,
     SENSOR_NAME_IMX260              = 109,
-	SENSOR_NAME_IMX386              = 110,
-    SENSOR_NAME_IMX258              = 111,
+    SENSOR_NAME_IMX258              = 110,
+	SENSOR_NAME_IMX386              = 111,
 
     SENSOR_NAME_SR261               = 201, // 201 ~ 300 Other vendor sensors
     SENSOR_NAME_SR544               = 203,
